9dc2bb9a011549f02997f5ce9db00486885f0938
Lance Ewing <lance.ewing@gmail.com>
2016-02-10 22:26:44 +0000

    Changed viewport to ExtendViewport and added camera adjustment to keep VIC screen aligned to top but centered horizontally.

--- core/src/emu/jvic/MachineScreen.java
+++ core/src/emu/jvic/MachineScreen.java
@@ -2,11 +2,15 @@ package emu.jvic;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.BufferUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * The main screen in the JVic emulator, i.e. the one that shows the video 
@@ -24,6 +28,10 @@ public class MachineScreen implements Screen {
   private SpriteBatch batch;
   private Texture screenTexture;
   private Pixmap screenPixmap;
+  private Viewport viewport;
+  private Camera camera;
+  private Camera cameraHUD;
+  
   
   /**
    * Constructor for MachineScreen.
@@ -34,6 +42,13 @@ public class MachineScreen implements Screen {
     batch = new SpriteBatch();
     screenPixmap = new Pixmap(machine.getMachineType().getTotalScreenWidth(), machine.getMachineType().getVisibleScreenHeight(), Pixmap.Format.RGBA8888);
     screenTexture = new Texture(screenPixmap, Pixmap.Format.RGBA8888, false);
+    
+    camera = new OrthographicCamera();
+    
+    // TODO: Decide if this is really needed. Surely if the main camera isn't actually moving around, we don't need a separate one for the UI.
+    cameraHUD = new OrthographicCamera();
+
+    viewport = new ExtendViewport(machine.getScreenWidth(), machine.getScreenHeight(), camera);
   }
   
   @Override
@@ -52,6 +67,10 @@ public class MachineScreen implements Screen {
   }
 
   private void draw() {
+    //camera.update();
+    
+    batch.setProjectionMatrix(camera.combined);
+    
     Gdx.gl.glClearColor(0, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     
@@ -62,14 +81,28 @@ public class MachineScreen implements Screen {
     BufferUtils.copy(machine.getFramePixels(), 0, screenPixmap.getPixels(), 77248);
     
     screenTexture.draw(screenPixmap, 0, 0);
-    
+
     batch.begin();
-    batch.draw(screenTexture, 0, 0, 408, 272, machine.getScreenLeft(), machine.getScreenTop(), 204, 272, false, false);
+    batch.draw(screenTexture, 
+        0,
+        0,
+        machine.getScreenWidth(), machine.getScreenHeight(), 
+        machine.getScreenLeft(), machine.getScreenTop(), 
+        machine.getScreenWidth() >> 1, machine.getScreenHeight(), 
+        false, false);    
     batch.end();
   }
   
   @Override
   public void resize(int width, int height) {
+    //viewport.update(width, height, true);
+    viewport.update(width, height, false);
+    
+    //align game box's top edge to top of screen
+    Camera camera = viewport.getCamera();
+    camera.position.x = machine.getScreenWidth() /2;
+    camera.position.y = machine.getScreenHeight() - viewport.getWorldHeight()/2;
+    camera.update();
   }
 
   @Override
