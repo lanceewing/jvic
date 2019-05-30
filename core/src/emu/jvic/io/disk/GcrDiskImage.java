4fac99c1a65dfc39ceae7f9955bb25b0eecd6409
lanceewing <lance.ewing@gmail.com>
2019-05-30 16:02:14 +0100

    Changed the GCR encoding of a sector to be more accurate. Added useful comments about GCR.

--- core/src/emu/jvic/io/disk/GcrDiskImage.java
+++ core/src/emu/jvic/io/disk/GcrDiskImage.java
@@ -5,11 +5,11 @@ package emu.jvic.io.disk;
  * 
  * @author Lance Ewing
  */
-public class GcrDiskImage {
-	
+public class GcrDiskImage { 
+  
   // Unlike MFM disks, the custom CBM CGR format has a variable number of sectors per track.
   private final int[][] TRACK_OFFSETS = new int[][] {
-	{ 0,    0,  0x00000 },  // There is no track 0, so this is a dummy entry.
+	  { 0,    0,  0x00000 },  // There is no track 0, so this is a dummy entry.
     { 21,   0,  0x00000 },
     { 21,  21,  0x01500 }, 
     { 21,  42,  0x02A00 },
@@ -51,10 +51,52 @@ public class GcrDiskImage {
     { 17, 734,  0x2DE00 },
     { 17, 751,  0x2EF00 }
   };
-	
-  // GCR_SECTOR_SIZE => 354
-  public final static int GCR_SECTOR_SIZE = 1 + 10 + 9 + 1 + 325 + 8;
-	
+  
+  // Constant for the size of a GCR encoded sector. We have 5 unencoded 0xFF sync mark bytes,
+  // then 10 encoded Header block bytes, then 9 unencoded 0x55 gap bytes, then another 5 
+  // unencoded 0xFF sync mark bytes, then 325 encoded Data Block bytes (256 * 5/4 = 320 is
+  // the data itself, and the other 5 are for the Data Block identifier, checksum, and two 
+  // off bytes), then comes another 8 unencoded 0x55 gap bytes (although note that the 
+  // length of the tail gap is variable and doesn't matter as long as it is at least 4 bytes).
+  public final static int GCR_SECTOR_SIZE = 5 + 10 + 9 + 5 + 325 + 8;
+  
+  // Some interesting notes about GCR:
+  //
+  //  When data is being read from a floppy diskette, the data is "clocked in" at a fixed rate.
+  //  A magnetic transition is interpreted as a "1" bit. The lack of a signal when data is expected
+  //  is interpreted as a "0" bit. Since the speed of the drive is not absolutely constant,
+  //  we can run into problems if there are too many "0" (no signal) bits in a row. Commodore's
+  //  GCR code is designed so that no GCR byte, or combination of GCR bytes,
+  //  ever contains more than two consecutive "0" bits. As a further precaution, the clock
+  //  is zeroed (cleared) every time a "1" bit is read. This re-synchronizes the clock to the
+  //  bit stream and prevents small fluctuations in the speed of the drive from causing read
+  //  errors. 
+  //
+  //  Note that the data recorded onto a diskette is not divided into bytes. There is just one
+  //  continuous stream of bits. In order to know where to begin to read or write bits, we
+  //  need some special identifying mark. This is the function of the SYNC mark, a string
+  //  of 10 or more 1s in a row. The GCR code is designed so that no combination
+  //  of bytes can produce more than eight "1" bits in a row. This guarantees the uniqueness
+  //  of the sync mark.
+  
+  //  To differentiate a sync mark from a normal data byte, the
+  //  1541 writes to diskette in two modes, a sync mode and a normal write mode.
+  //  To appreciate the uniqueness of a sync mark we must first look at how a normal data
+  //  byte is recorded. During normal write mode each 8-bit byte is encoded into 10 bits before
+  //  it is written to disk. Commodore calls this encoding scheme binary to GCR (Group Code
+  //  Recording) conversion. 
+  
+  //  A sync mark is 10 or more on bits (ls) recorded in succession. Only
+  //  one normal data byte, an $FF (%11111111), can even begin to fill the shoes of a sync
+  //  mark. During normal write mode, however, an $FF would take the following GCR form,
+  //  1010110101. Enter sync mode. When the 1541 writes an $FF in sync mode no binary
+  //  to GCR conversion is done. A single $FF is only eight consecutive on bits and falls short
+  //  of the ten consecutive on bits needed to create a sync character. To remedy this, Commodore
+  //  writes five consecutive 8-bit $FFs to disk. This records 40 on bits (ls) in succession.
+  //  the overkill is intentional on the DOS's part. Commodore is trying to guarantee
+  //  that the 1541 will never have any trouble finding a sync mark during subsequent
+  //  reads/writes to a diskette. 
+  
   // GCR conversion table - used for converting ordinary byte to 10-bits (or 4 bits to 5)
   private final int[] GCR = new int[] {
     0x0a, 0x0b, 0x12, 0x13,
@@ -62,7 +104,7 @@ public class GcrDiskImage {
     0x09, 0x19, 0x1a, 0x1b,
     0x0d, 0x1d, 0x1e, 0x15
   };
-	
+  
   // 5 bits > 4 bits (0xff => invalid)
   private final int[] GCR_REV = new int[] {
     0xff, 0xff, 0xff, 0xff, // 0 - 3invalid...
@@ -75,13 +117,13 @@ public class GcrDiskImage {
     0xff, 0x09, 0x0a, 0x0b, // 18 invalid...
     0xff, 0x0d, 0x0e, 0xff, // 1c, 1f invalid...
   };
-	
+  
   private int numOfTracks;           // Number of tracks on the disk.
   private int[] rawImage;            // The raw disk image file loaded into memory
   private int diskID1;
   private int diskID2;
   private Sector[][] allTracks;
-	
+  
   /**
    * Constructor for GcrDiskImage.
    * 
@@ -144,10 +186,10 @@ public class GcrDiskImage {
    * @param sectors The array of Sectors to GCR encode.
    */
   private void encodeTrack(Sector[] sectors) {
-	for (int sectorNum=0; sectorNum<sectors.length; sectorNum++) {
-	  Sector sector = sectors[sectorNum];
-	  sector.gcrData = buildGCRSectorData(sector.trackNum, sectorNum, sector.rawData);
-	}
+    for (int sectorNum=0; sectorNum<sectors.length; sectorNum++) {
+      Sector sector = sectors[sectorNum];
+      sector.gcrData = buildGCRSectorData(sector.trackNum, sectorNum, sector.rawData);
+    }
   }
   
   /**
@@ -166,7 +208,8 @@ public class GcrDiskImage {
   }
   
   /**
-   * Builds the GCR encoded data for a sector.
+   * Builds the Commodore GCR encoded data for a sector. We need to do this because the .d64
+   * disk image is already decoded, but the DOS ROM code expects the data to be GCR encoded.
    * 
    * @param track The track number for the sector.
    * @param sector The sector number of the sector within that track.
@@ -175,52 +218,74 @@ public class GcrDiskImage {
    * @return The GCR encoded sector data.
    */
   private int[] buildGCRSectorData(int track, int sector, int[] rawData) {
-	int[] gcrData = new int[GCR_SECTOR_SIZE];
-	int pos = 0;
-	int cSum = 0;
+    int[] gcrData = new int[GCR_SECTOR_SIZE];
+    int pos = 0;
+    int cSum = 0;
 
-	// First data is sync!
-	gcrData[pos++] = 0xff;
+    // Any given sector is divided into two contiguous parts: a header block and a data block.
+    
+    // ----------------------------------------- Header Block --------------------------------------
+    
+    //       5           Sync Character (DOS writes out 5 0xFF chars, but only 2 is strictly needed)
+    for (int i = 0; i < 5; i++) {
+      gcrData[pos++] = 0xff;
+    }
+    //       1           Header Block Identifier ($08)
+    //       1           Header Block Checksum: The checksum is the XOR of C, S, T and ID
+    //       1           Sector Number: This is the sector number (0-16/17/18/20, depending on the track). 
+    //       1           Track Number: This is the track number (1-35). It is the same for all sectors on the same track.
+    makeGCR(gcrData, pos, 0x08, sector ^ track ^ diskID1 ^ diskID2, sector, track);
+    pos += 5;
+    //       1           ID LO : The two-byte ID should be unique per disk and is used to detect disk 
+    //       1           ID HI : changes. It is the same for all sectors on the same disk.
+    //       2           Off Bytes ($0F)
+    makeGCR(gcrData, pos, diskID2, diskID1, 0x0f, 0x0f);
+    pos += 5;
+    //       9           Header Gap ($55). Separates header from sector data.
+    for (int i = 0; i < 9; i++) {
+      gcrData[pos++] = 0x55;   // Note: This is not GCR encoded.
+    }
 
-	makeGCR(gcrData, pos, 0x08, sector ^ track ^ diskID1 ^ diskID2, sector, track);
-	pos += 5;
-	makeGCR(gcrData, pos, diskID2, diskID1, 0x0f, 0x0f);
-	pos += 5;
+    // ----------------------------------------- Data Block --------------------------------------
+    
+    //       -           Sync Character (not GCR encoded)
+    for (int i = 0; i < 5; i++) {
+      gcrData[pos++] = 0xff;
+    }
+    
+    //       1           Data Block Identifier ($07)
+    //       3           First 3 Data Bytes
 
-	// pos = 11
-	for (int i = 0; i < 9; i++) {
-	  gcrData[pos++] = 0x55;
-	}
+    // Cancel out first 7 by setting cSum to 7.
+    cSum = 0x07;
+    cSum ^= makeGCR(gcrData, pos, 0x07, rawData[0] & 0xff, rawData[1] & 0xff, rawData[2] & 0xff);
+    pos += 5;
 
-	// Another sync - at position 20 (?)
-	gcrData[pos++] = 0xff;
-	// pos = 21
-	// cancel out first 7 by setting cSum to 7
-	cSum = 0x07;
-	cSum ^= makeGCR(gcrData, pos, 0x07, rawData[0] & 0xff, rawData[1] & 0xff, rawData[2] & 0xff);
-	pos += 5;
-
-	// pos = 26
-	for (int i = 3, n = 255; i < n; i += 4) {
-	  cSum ^= makeGCR(
+    //       252         Next 252 Data Bytes
+    for (int i = 3, n = 255; i < n; i += 4) {
+      cSum ^= makeGCR(
           gcrData, pos, 
           rawData[i + 0] & 0xff, 
           rawData[i + 1] & 0xff,
           rawData[i + 2] & 0xff,
           rawData[i + 3] & 0xff);
-	  pos += 5;
-	}
+      pos += 5;
+    }
 
-	cSum ^= rawData[255] & 0xff;
+    cSum ^= rawData[255] & 0xff;
+    
+    //       1           Last Data Byte
+    //       1           Data Block Checksum
+    //       2           Off Bytes ($00)
+    makeGCR(gcrData, pos, rawData[255] & 0xff, cSum & 0xff, 0, 0);
+    pos += 5;
 
-	makeGCR(gcrData, pos, rawData[255] & 0xff, cSum & 0xff, 0, 0);
-	pos += 5;
+    //       Variable    Tail Gap ($55). Not GCR encoded. Length normally varies, but we use 8.
+    for (int i = 0; i < 8; i++) {
+      gcrData[pos++] = 0x55;
+    }
 
-	for (int i = 0, n = 8; i < n; i++) {
-	  gcrData[pos++] = 0x55;
-	}
-
-	return gcrData;
+    return gcrData;
   }
   
   /**
@@ -270,7 +335,7 @@ public class GcrDiskImage {
    * @return The number of sectors in the given track.
    */
   public int getSectorCount(int track) {
-	return TRACK_OFFSETS[track][0];
+    return TRACK_OFFSETS[track][0];
   }
   
   /**
