a89bfe7adb9c0bede07cc51064ef22ec055d8b61
Lance Ewing <lance.ewing@gmail.com>
2025-11-22 16:27:25 +0000

    Batch of source reformatting, to 4 space indent.

--- core/src/main/java/emu/jvic/BaseChip.java
+++ core/src/main/java/emu/jvic/BaseChip.java
@@ -9,32 +9,32 @@ import emu.jvic.memory.*;
  */
 public abstract class BaseChip {
 
-  /**
-   * Holds a reference to the machine's memory map.
-   */
-  protected Memory memory;
-  
-  /**
-   * Holds a direct reference to the int array holding the machine's memory. This
-   * is often used for faster reading and writing when it is safe to do so.
-   */
-  protected int mem[];
+    /**
+     * Holds a reference to the machine's memory map.
+     */
+    protected Memory memory;
 
-  /**
-   * Holds an array of references to instances of MemoryMappedChip where each
-   * instance determines the behaviour of reading or writing to the given memory
-   * address.
-   */
-  protected MemoryMappedChip memoryMap[];
-  
-  /**
-   * Sets a reference to the VIC 20 memory map. 
-   *  
-   * @param memory The VIC 20 memory map.
-   */
-  public void setMemory(Memory memory) {
-    this.memory = memory;
-    this.mem = memory.getMemoryArray();
-    this.memoryMap = memory.getMemoryMap();
-  }
+    /**
+     * Holds a direct reference to the int array holding the machine's memory. This
+     * is often used for faster reading and writing when it is safe to do so.
+     */
+    protected int mem[];
+
+    /**
+     * Holds an array of references to instances of MemoryMappedChip where each
+     * instance determines the behaviour of reading or writing to the given memory
+     * address.
+     */
+    protected MemoryMappedChip memoryMap[];
+
+    /**
+     * Sets a reference to the VIC 20 memory map.
+     * 
+     * @param memory The VIC 20 memory map.
+     */
+    public void setMemory(Memory memory) {
+        this.memory = memory;
+        this.mem = memory.getMemoryArray();
+        this.memoryMap = memory.getMemoryMap();
+    }
 }
--- core/src/main/java/emu/jvic/config/AppConfig.java
+++ core/src/main/java/emu/jvic/config/AppConfig.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 
 public class AppConfig {
 
-  private ArrayList<AppConfigItem> apps;
+    private ArrayList<AppConfigItem> apps;
 
-  public AppConfig() {
-    this.apps = new ArrayList<AppConfigItem>();
-  }
-  
-  /**
-   * @return the apps
-   */
-  public ArrayList<AppConfigItem> getApps() {
-    return apps;
-  }
+    public AppConfig() {
+        this.apps = new ArrayList<AppConfigItem>();
+    }
 
-  /**
-   * @param apps the apps to set
-   */
-  public void setApps(ArrayList<AppConfigItem> apps) {
-    this.apps = apps;
-  }
+    /**
+     * @return the apps
+     */
+    public ArrayList<AppConfigItem> getApps() {
+        return apps;
+    }
+
+    /**
+     * @param apps the apps to set
+     */
+    public void setApps(ArrayList<AppConfigItem> apps) {
+        this.apps = apps;
+    }
 }
