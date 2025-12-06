450761b64580c85c7fb650a1bcd6c2b03ec7df7f
Lance Ewing <lance.ewing@gmail.com>
2025-12-06 21:12:25 +0000

    Adding another site to supported fetch sites.

--- html/webapp/functions/programs.js
+++ html/webapp/functions/programs.js
@@ -23,6 +23,7 @@ export async function onRequest(context) {
              targetUrl.startsWith("https://www.zimmers.net/") ||
 			 targetUrl.startsWith("http://viznut.fi/") ||
 			 targetUrl.startsWith("https://files.scene.org/") ||
+			 targetUrl.startsWith("http://www.evilshirt.com/") ||
 			 targetUrl.startsWith("https://sourceforge.net/p/vice-emu/code/HEAD/tree/testprogs/VIC20/") ||
              targetUrl.startsWith("https://archive.org/"))) {
 
